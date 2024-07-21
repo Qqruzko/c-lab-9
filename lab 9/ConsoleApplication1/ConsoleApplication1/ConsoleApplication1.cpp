5102196096b40306bdccbbb3a34d676977f1e98f
Krasnopevtsev Maxim <150928737+Qqruzko@users.noreply.github.com>
2024-07-21 18:00:39 +0300

    Add files via upload

---
+++ lab 9/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+#include <cmath>
+class Triangle
+{
+public:
+    int a, b, c;
+    Triangle(int a_in, int b_in, int c_in)
+    {
+        a = a_in;
+        b = b_in;
+        c = c_in;
+    }
+
+    void ploschad()
+    {
+        
+        
+
+        try {
+           
+            if ((a > b + c) || (b  > a + c) || (c > a + b))
+            {
+                throw std::runtime_error("Одна сторона больше суммы двух других");
+                
+            }
+            int p = a + b + c;
+            int result = sqrt((p * (p - a) * (p - b) * (p - c)));
+            std::cout << "Result: " << result << std::endl;
+        }
+        catch (const std::exception& e) {
+            // Exception handling code
+            std::cout << "Exception caught: " << e.what() << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Triangle triangle1(2,2,2);
+    triangle1.ploschad();
+}
+
---
+++ lab 9/ConsoleApplication1/time safe/time safe.cpp
@@ -0,0 +1,56 @@
+﻿
+#include <iostream>
+
+class time_error : public std::exception
+{
+public:
+    time_error(const std::string& message) : message{ message }
+    {}
+    const char* what() const noexcept override
+    {
+        return message.c_str();     
+    }
+private:
+    std::string message;   
+};
+
+class Time
+{
+public:
+    Time(int hours, int minutes, int seconds)
+    {
+        if (minutes > 60)    
+            throw time_error("Invalid minutes");
+        this->hours = hours;
+        this->minutes = minutes;
+        this->seconds = seconds;
+    }
+    void print() const
+    {
+        std::cout << "hours: " << hours << "\tminutes: " << minutes << "\tseconds: " << seconds << std::endl;
+    }
+private:
+    int hours,  minutes,  seconds;
+};
+
+void testTime(int hours, int minutes, int seconds)
+{
+    try
+    {
+        Time person{ hours, minutes, seconds};   
+        person.print();
+    }
+    catch (const time_error& err) 
+    {
+        std::cout << "Person error: " << err.what() << std::endl;
+    }
+    catch (const std::exception&)  
+    {
+        std::cout << "Something wrong" << std::endl;
+    }
+}
+int main()
+{
+    testTime(35,60,23);  
+    
+}
